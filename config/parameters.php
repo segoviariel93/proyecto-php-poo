8479cb387b8bde52a22128c4138b1eb01f993057
ARiel <segoviariel93@gmail.com>
2021-01-14 10:38:50 -0500

    cargar estilos publicos

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
-define("base_url", "http://localhost:83/proyecto-php-poo/");
+define("base_url", "http://192.168.0.233:83/proyecto-php-poo/");
 define("controller_default", "usuarioController");
 define("action_default", "index");
 
