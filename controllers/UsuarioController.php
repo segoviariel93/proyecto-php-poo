bffe6c1ea5df5d439cb30c3228dd6456d14e87f4
segoviariel93@gmail.com <segoviariel93@gmail.com>
2021-01-14 22:39:39 -0500

    test

--- controllers/UsuarioController.php
+++ controllers/UsuarioController.php
@@ -19,6 +19,11 @@ class usuarioController{
 			$password = (isset($_POST['password']) && $_POST['password']!="") ? $_POST['password'] : false;
 
 			
+			var_dump($nombre);
+			var_dump($apellidos);
+			var_dump($email);
+			var_dump($password);
+			die();
 			
 			if($nombre && $apellidos && $email && $password){
 				$usuario = new Usuario();
